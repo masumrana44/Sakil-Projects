a5065c9409dc14aebffe508b5a637509a8050329
Masum-Rana <masumrana446267@gmail.com>
2022-11-25 19:37:19 +0600

    array

---
+++ index.js
@@ -0,0 +1,72 @@
+// 1.What is array ?
+
+// 3.How to declere array ?
+//2. what is index ?
+// 0 theke index ta shuru hoy
+// 4.What is Array Length?
+
+// var index = [0, 1, 2, 3, 4, 5, 6, 7];
+// var ShowCase = ["Glasses", "Plate", "cup", "Dinner Set"];
+// var myArrayLength = ShowCase.length;
+// console.log(myArrayLength)
+// console.log(`Mejjan asche ${ShowCase[2]} kore Caa dew`)
+
+// how to get array Element 
+// var myGlass = ShowCase[0];
+// // console.log(myGlass)
+// var myPlate = ShowCase[1];
+// console.log(myPlate)
+
+
+// how to set element inside the a array
+
+var celebraties = ['shakib khan', 'Jayed Khan', 'Hero Alom', 'Anonto Jolil', 'Raj'];
+celebraties.pop();
+console.log(celebraties)
+var nayoksLength = celebraties.length;
+// console.log(nayoksLength)
+
+//  Re-assign / set Element inside the array 
+celebraties[2] = 'Riyaj Hossain';
+celebraties[3] = 'Borsha ';
+celebraties[4] = 'Porimoni';
+
+// set Element inside the array 
+var myPorimoni = celebraties[4];
+// console.log(myPorimoni)
+
+//
+// console.log(celebraties)
+
+
+// var myBioData = ['Masum Rana', 24, 'Al-Arapha Academy'];
+
+// // pop  that's means :-last element ke Vanish kore debo
+// myBioData[3] = 'Rustam Ali Degree College'
+// myBioData[4]=
+// myBioData[2] = 'Haidargonj Model School';
+// console.log(myBioData)
+
+// array pop
+// myBioData.pop()
+// console.log(myBioData)
+
+// //  array push
+// myBioData.push('Haidargonj Model School');
+// // console.log(myBioData)
+
+
+
+var naykas = ['Porimoni', 'Borsha', 'Sabana', 'Mahia Mahi', 'Bobita'];
+
+// Shift that's means-:Vanish the array's first element 
+naykas.shift();
+
+// unshift that's means-:set array's element in the first 
+naykas.unshift('Kobori');
+
+console.log(naykas)
+
+
+
+
